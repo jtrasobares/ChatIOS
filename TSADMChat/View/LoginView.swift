750040fe61dcda0c2be34d81709713a79f69960e
JOSE IGNACIO TRASOBARES IBOR <alu.100495@usj.es>
2024-02-05 20:35:48 +0100

    Add Login page

---
+++ TSADMChat/View/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  TSADMChat
+//
+//  Created by Jose Ignacio Trasobares Ibor on 5/2/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
